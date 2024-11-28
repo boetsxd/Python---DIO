2f0f1297c3b6b474415fe334685e993e60eed145
Alex Silva <lex.16@hotmail.com>
2024-11-28 11:06:59 -0300

    Operadores Lógicos
    Operadores Lógicos

---
+++ operadores_logicos.py
@@ -0,0 +1,15 @@
+saldo = 1000
+saque = 200
+limite = 100
+conta_especial = True
+
+print(True and True)
+print(True and False)
+print(False and False)
+print(True or True)
+print(True or False)
+print(False or False)
+
+print((saldo >= saque) and (saque <= limite))
+
+print((saldo >= saque) or (saque <= limite))
