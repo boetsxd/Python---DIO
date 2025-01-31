e69287384851677b0dd38dec1ad8b52e06c9a6dc
Alex Silva <lex.16@hotmail.com>
2025-01-31 17:00:06 -0300

    Concluindo código.

--- Testes de código/1-6.py
+++ Testes de código/1-6.py
@@ -2,11 +2,14 @@
 # - Se dois números fornecidos pelo usuário são iguais.
 # - Se o primeiro é maior ou menor que o segundo.
 
-num1 = int(input("Informe o primeiro número: "))
-num2 = int(input("Informe o segundo número: "))
+try:
+    num1 = int(input("Informe o primeiro número: "))
+    num2 = int(input("Informe o segundo número: "))
 
-if num1 != num2:
-    compare = "maior" if num1 > num2 else "menor"
-    print(f"Estes números são diferentes e {num1} é {compare} que {num2}")
-else:
-    print(f"Estes números são iguais")
+    if num1 != num2:
+        compare = "maior" if num1 > num2 else "menor"
+        print(f"Os números são diferentes: {num1} é {compare} que {num2}")
+    else:
+        print(f"Os números são iguais!")
+except ValueError:
+    print("Por favor, informe apenas números inteiros")
