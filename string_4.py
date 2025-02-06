9af9b14fd7bc0539035494e52666b9520db93fb5
Alex Silva <lex.16@hotmail.com>
2025-02-06 20:26:26 -0300

    String Múltiplas linhas

---
+++ string_4.py
@@ -0,0 +1,21 @@
+nome = "Alex"
+
+mensagem = f"""
+    Olá, meu nome é {nome},
+Eu estou aprendendo Python na DIO.
+      Essa mensagem tem diferentes recursos
+"""
+print(mensagem)
+
+print(
+    """
+    ===================MENU=================
+
+    1 = Depositar
+    2 = Sacar
+    0 = Sair    
+    
+    ========================================
+    
+    """
+)
